4a5f534591f7853d898591ba796dfdc6cff8bf94
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-12-05 22:50:10 -0800

    Update start.py

--- program/start.py
+++ program/start.py
@@ -81,7 +81,6 @@ async def start_(client: Client, message: Message):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
                     ),
                 ],
-                ],
             ]
         ),
         disable_web_page_preview=True,
